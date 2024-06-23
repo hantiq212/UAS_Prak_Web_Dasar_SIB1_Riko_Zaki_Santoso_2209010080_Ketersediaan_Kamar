947d0f32b3bae366b88efa1c42ce62ff9067ec65
hantiq212 <116475863+hantiq212@users.noreply.github.com>
2024-06-23 18:53:39 +0700

    Create 2024_06_23_115142_create_bookings_table.php

---
+++ database/migrations/2024_06_23_115142_create_bookings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_ruangan')->constrained('ruangans');
+            $table->foreignId('id_pasien')->constrained('pasiens');
+            $table->date('tanggal_check_in');
+            $table->date('tanggal_check_out')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
